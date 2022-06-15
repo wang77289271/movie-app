9808f0fa982f10309e4add707dc6babc9ca9987d
Shawn Wang <wang77289271@gmail.com>
2022-06-15 15:24:13 -0600

    nav search layout

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Nav.css'
+import SearchIcon from '@mui/icons-material/Search'
 
 const Nav = () => {
   return (
@@ -7,8 +8,14 @@ const Nav = () => {
       <div className="logo">
         <h1>yMovie</h1>
       </div>
-      <div className="search">
-        <input type="text" className="search" />
+      <div className="search-container">
+        <div className="search">
+          <input type="text" className="search" placeholder="Search..." />
+          <span className="search-vline"></span>
+          <div className="search-icon">
+            <SearchIcon />
+          </div>
+        </div>
       </div>
     </div>
   )
